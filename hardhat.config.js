99617d2fb13f2d513c4d376fcfcacfc59328b3d4
Juuddi <98721462+Juuddi@users.noreply.github.com>
2022-11-15 10:24:47 -0600

    Update hardhat.config.js
    Working on updating quais to use contract grinding
    
    Bump chain ID and local quais
    
    Add constructor arg

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,19 +11,11 @@ const { RPC_URL, PRIVATE_KEY, ROPSTEN_URL } = process.env; // import private key
 module.exports = {
   defaultNetwork: "ropsten",
   networks: {
-    // quaitestnet is the default quai network during the entire development phase.
-    quaitestnet: {
-      url: RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 994, // Mainnet (technically testnet right now) chainId
-      websocket: true,
-      gas: 2000000, // gas limit used for deploys. This is an arbitrary value, accurate gas estimates must be obtained for deployments.
-    },
     // ropsten is a locally ran testnet of all 13 of the quai network blockchains.
     ropsten: {
       url: ROPSTEN_URL,
       accounts: [PRIVATE_KEY],
-      chainId: 3, // Ropsten chainId
+      chainId: 1337, // Ropsten chainId
       websocket: true,
       gas: 2000000, // gas limit used for deploys. This is an arbitrary value, accurate gas estimates must be obtained for deployments.
     },
@@ -44,6 +36,6 @@ module.exports = {
     artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 200000000000,
+    timeout: 20000,
   },
 };
