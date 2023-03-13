f89acefc6f03828733c658f8a84e6336fc3f5ab1
Juuddi <justinrudi26@gmail.com>
2023-03-13 14:15:38 -0500

    Save progress

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,29 @@
 const quais = require("quais");
 const hre = require("hardhat");
 
-
 async function main() {
   const Greeter = await hre.ethers.getContractFactory("Greeter");
-  console.log("Deploying Greeter...")
+  console.log("Deploying Greeter...");
 
   // //to create 'signer' object;here 'account'
-  const provider = new quais.providers.JsonRpcProvider(hre.config.networks.ropsten.url);
-  const walletWithProvider = new quais.Wallet(hre.config.networks.ropsten.accounts[0], provider);
+  const provider = new quais.providers.JsonRpcProvider(
+    hre.config.networks.quai.url
+  );
+  const walletWithProvider = new quais.Wallet(
+    hre.config.networks.quai.accounts[0],
+    provider
+  );
   await provider.ready;
 
   // const contractBytes = await grindContractAddress(nonce, "zone-0-0", walletWithProvider.address, Greeter);
-  const myContract = new quais.ContractFactory(Greeter.interface, Greeter.bytecode, walletWithProvider);
-  
+  const myContract = new quais.ContractFactory(
+    Greeter.interface,
+    Greeter.bytecode,
+    walletWithProvider
+  );
+
   // // If your contract requires constructor args, you can specify them here
-  const greeter = await myContract.deploy("hello");
+  const greeter = await myContract.deploy("Sup bitches we deployed a Quai Contract");
 
   await greeter.deployed();
   console.log("Greeter deployed to:", greeter.address);
