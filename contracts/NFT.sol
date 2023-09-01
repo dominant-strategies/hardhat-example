bff99cfb4fc70fb356420785bd3c2181bc2a700a
Juuddi <justinrudi26@gmail.com>
2023-09-01 15:32:42 -0500

    remove extraneous contracts and add import for quai-hardhat

--- contracts/NFT.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract MyNFT is ERC721 {
-    constructor() ERC721("New Cool NFT", "MyNFT") {}
-}
