417347328481c062358bc3116303bd9a81962e81
Judi <98721462+Juuddi@users.noreply.github.com>
2023-10-25 13:31:57 -0500

    Fix hardhat.config (#18)

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,7 @@ module.exports = {
 	defaultNetwork: 'cyprus1',
 	networks: {
 		cyprus1: {
-			url: process.env.CYPRUS1URL.toString(),
+			url: `${process.env.CYPRUS2URL}`,
 			accounts: [process.env.CYPRUS1PK],
 			chainId: chainId,
 		},
